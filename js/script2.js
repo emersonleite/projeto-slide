afcbb73421f450384b586be767fabbe1a9d632f4
Emerson Leite <emerson@unna.eng.br>
2020-02-07 21:09:59 -0300

    202002072110

--- js/script2.js
+++ js/script2.js
@@ -3,7 +3,5 @@ import SlideNav from './slide2.js';
 const slide = new SlideNav('.slide', '.slide-wrapper');
 slide.init();
 slide.addArrow('.prev', '.next');
-
-
 slide.addControl('.custom-controls');
 
